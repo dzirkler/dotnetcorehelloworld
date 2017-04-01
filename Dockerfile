9922f90f927b7dc58382ab1445e78bfe1cdf829a
Damon Zirkler <dzirkler@steelcase.com>
2017-04-01 14:54:20 -0700

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
 FROM microsoft/dotnet:1.1.1-runtime
 
-# COPY . /src
-# WORKDIR /src
-# RUN /src/install.sh
-# WORKDIR /src/hwapp
-# RUN dotnet restore && \
-#     dotnet publish -c Release -o /app && \
-#     rm -fr /src 
-
 COPY ./hwapp/out /app
 
 ENTRYPOINT ["dotnet", "/app/hwapp.dll"]
--- build.sh
+++ build.sh
@@ -4,8 +4,3 @@ set -e
 rm -fr ./out
 dotnet restore ./hwapp
 dotnet publish -c Release -o ./out ./hwapp
-
-
-
-#/Users/dzirkler/Projects/experiments/dotnetcorehelloworld
-#docker run -it --rm -v '/Users/dzirkler/Projects/experiments/dotnetcorehelloworld:/src' microsoft/dotnet:1.1.1-sdk sh /src/build.sh
